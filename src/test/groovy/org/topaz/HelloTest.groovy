3cfd7e359e943955e6ed47cdfb3afecd276d0580
Max Baird <mmb1@hw.ac.uk>
2020-04-12 16:53:23 +0100

    Removed test directory

--- src/test/groovy/org/topaz/HelloTest.groovy
+++
@@ -1,7 +0,0 @@
-package org.topaz
-
-class HelloTest extends GroovyTestCase {
-    void 'test Topaz should return "Hello, World!"' () {
-        assert new Topaz().world == "Hello, World!"
-    }
-}
