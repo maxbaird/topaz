eae8653123e191ce84bac1aedeead85809ac4e69
Max Baird <mmb1@hw.ac.uk>
2020-04-13 01:23:36 +0100

    Implemented the flags for the register class

--- src/main/groovy/org/topaz/Register.groovy
+++ src/main/groovy/org/topaz/Register.groovy
@@ -1,19 +1,37 @@
 class Register{
-  def A
-  def B
-  def C
-  def D
-  def E
-  def F
-  def H
-  def L
+  int A
+  int B
+  int C
+  int D
+  int E
+  int F
+  int H
+  int L
 
-  private def combine(def b1, def b2){
-   return b1 << 8 | b2
-  }
+  /* flag bits */
+  private static int FLAG_Z = 7
+  private static int FLAG_N = 6
+  private static int FLAG_H = 5
+  private static int FLAG_C = 4
+
+ private void setFlag(int pos){
+  this.F = this.F | (1 << pos)
+ }
+
+ private void clearFlag(int pos){
+  this.F = ~(1 << pos) & this.F & 0xFF
+ }
+
+ private boolean isSet(int pos){
+  return this.F & (1 << pos)
+ }
+
+ private def combine(def b1, def b2){
+  return b1 << 8 | b2
+ }
 
  private def split(def s){
-   def b1 = s >> 8
+  def b1 = s >> 8
    def b2 = s & 0xFF
    return [b1, b2]
  }
@@ -48,6 +66,42 @@ class Register{
 
  void setHL(def s){
   (H, L) = split(s)
+ }
+
+ boolean isZ(){
+  return isSet(FLAG_Z)
+ }
+
+ boolean isN(){
+  return isSet(FLAG_N)
+ }
+
+ boolean isH(){
+  return isSet(FLAG_H)
+ }
+
+ boolean isC(){
+  return isSet(FLAG_C)
+ }
+
+ void setZ(boolean z){
+  z ? setFlag(FLAG_Z) : clearFlag(FLAG_Z)
+ }
+
+ void setN(boolean n){
+  n ? setFlag(FLAG_N) : clearFlag(FLAG_N)
+ }
+
+ void setH(boolean h){
+  h ? setFlag(FLAG_H) : clearFlag(FLAG_H)
+ }
+
+ void setC(boolean c){
+  c ? setFlag(FLAG_C) : clearFlag(FLAG_C)
+ }
+
+ def printFlags(){
+  println String.format("%8s", Integer.toBinaryString(this.F)).replaceAll(" ", "0")
  }
 
 }
--- src/main/groovy/org/topaz/Topaz.groovy
+++ src/main/groovy/org/topaz/Topaz.groovy
@@ -1,10 +1,15 @@
 package org.topaz
 
 import Cartridge
+import Register
 
 class Topaz{
 	static main(args) {
 	 def cartridge = Cartridge.load("/home/maxx/Documents/alba.pdf")
   println cartridge.length
+  def reg = new Register()
+
+  reg.setZ(true)
+  println reg.printFlags()
 	}
 }
