d7162de76f8c5498c659c90793543e22b7f9cc8b
Max Baird <mmb1@hw.ac.uk>
2020-06-21 23:55:36 +0100

    Added appropriate getters and setters for new register types. Need to fix the CPU class to use these

--- src/main/groovy/org/topaz/cpu/Register2.java
+++ src/main/groovy/org/topaz/cpu/Register2.java
@@ -1,14 +1,16 @@
 package org.topaz.cpu;
+import org.topaz.util.UInt;
 
 public class Register2{
-    public int A;
-    public int B;
-    public int C;
-    public int D;
-    public int E;
-    public int F;
-    public int H;
-    public int L;
+
+    public UInt A = new UInt(UInt.EIGHT_BITS);
+    public UInt B = new UInt(UInt.EIGHT_BITS);
+    public UInt C = new UInt(UInt.EIGHT_BITS);
+    public UInt D = new UInt(UInt.EIGHT_BITS);
+    public UInt E = new UInt(UInt.EIGHT_BITS);
+    public UInt F = new UInt(UInt.EIGHT_BITS);
+    public UInt H = new UInt(UInt.EIGHT_BITS);
+    public UInt L = new UInt(UInt.EIGHT_BITS);
 
     public int pc;
     public int sp;
@@ -35,15 +37,15 @@ public class Register2{
     }
     
     private void setFlag(int pos){
-        this.F = (this.F | (1 << pos)) & 0xFF;
+        this.F.setValue((this.F.getValue() | (1 << pos)) & 0xFF);
     }
 
     private void clearFlag(int pos){
-        this.F = ~(1 << pos) & this.F & 0xFF;
+        this.F.setValue(~(1 << pos) & this.F.getValue() & 0xFF);
     }
 
     private boolean isSet(int pos){
-        return ((this.F >> pos) & 1) == 1;
+        return ((this.F.getValue() >> pos) & 1) == 1;
     }
 
     private int combine(int b1, int b2){
@@ -66,47 +68,47 @@ public class Register2{
     }
 
     public int getAF(){
-        return combine(A,F);
+        return combine(A.getValue(),F.getValue());
     }
 
     public int getBC(){
-        return combine(B,C);
+        return combine(B.getValue(),C.getValue());
     }
 
     public int getDE(){
-        return combine(D,E);
+        return combine(D.getValue(),E.getValue());
     }
 
     public int getHL(){
-        return combine(H,L);
+        return combine(H.getValue(),L.getValue());
     }
 
     public void setAF(int s){
         int b[] = new int[2];
         b = split(s); 
-        A = b[0];
-        F = b[1];
+        A.setValue(b[0]);
+        F.setValue(b[1]);
     }
 
     public void setBC(int s){
         int b[] = new int[2];
         b = split(s); 
-        B = b[0];
-        C = b[1];
+        B.setValue(b[0]);
+        C.setValue(b[1]);
     }
 
     public void setDE(int s){
         int b[] = new int[2];
         b = split(s); 
-        D = b[0];
-        E = b[1];
+        D.setValue(b[0]);
+        E.setValue(b[1]);
     }
 
     public void setHL(int s){
         int b[] = new int[2];
         b = split(s); 
-        H = b[0];
-        L = b[1];
+        H.setValue(b[0]);
+        L.setValue(b[1]);
     }
 
     public boolean isZ(){
@@ -190,11 +192,74 @@ public class Register2{
     }
 
     public void clearAllFlags() {
-        F = 0;
+        F.setValue(0);
     }
 
+    public int getA() {
+        return A.getValue();
+    }
+
+    public void setA(int a) {
+        A.setValue(a);;
+    }
+
+    public int getB() {
+        return B.getValue();
+    }
+
+    public void setB(int b) {
+        B.setValue(b);
+    }
+
+    public int getC() {
+        return C.getValue();
+    }
+
+    public void setC(int c) {
+        C.setValue(c);
+    }
+
+    public int getD() {
+        return D.getValue();
+    }
+
+    public void setD(int d) {
+        D.setValue(d);
+    }
+
+    public int getE() {
+        return E.getValue();
+    }
+
+    public void setE(int e) {
+        E.setValue(e);
+    }
+
+    public int getF() {
+        return F.getValue();
+    }
+
+    public void setF(int f) {
+        F.setValue(f);
+    }
+
+    public int getH() {
+        return H.getValue();
+    }
+
+    public void setH(int h) {
+        H.setValue(h);
+    }
+
+    public int getL() {
+        return L.getValue();
+    }
+
+    public void setL(int l) {
+        L.setValue(l);
+    }
     public void printFlags(){
-        System.out.println(String.format("%8s", Integer.toBinaryString(this.F)).replaceAll(" ", "0"));
+        System.out.println(String.format("%8s", Integer.toBinaryString(this.F.getValue())).replaceAll(" ", "0"));
     }
 
     @Override
